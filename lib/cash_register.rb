f67df7cd98b1457296eca35195e405b1c4e93183
Christian V <viray.christianj@gmail.com>
2019-01-29 06:48:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
+def CashRegister 
+  
+  attr_accessor :total, :discount, :item, :price
+  
+  @@cash_register = 0 
+  
+  def initialize(discount = 0)
+    @total = 0 
+    @discount = discount 
+    @items = []
+  end   
+  
+  def self.total
+    @total
+  end 
+    
+  def add_item(item, price, quantity = 1)
+    @total = price * quantity 
+    quantity.times do 
+    @items << item
+    
+    
+  end 
+  
+  
+  
+  def apply_discount
+  end 
+  
+  
+  def void_last_transaction
+    @total = @total
+  end 
+  
+  
+end 
 
