716cc3523fb7261daf4afcc5097bf62671fcd5c0
Christian V <viray.christianj@gmail.com>
2019-01-29 06:59:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,10 +14,10 @@ def CashRegister
     @total
   end 
     
-  def add_item(item, price, quantity = 1)
+  def add_item(name, price, quantity = 1)
     @total += price * quantity 
     quantity.times do 
-    @items << item
+    @items << name
   end 
     @last_transaction = price * quantity
   end 
